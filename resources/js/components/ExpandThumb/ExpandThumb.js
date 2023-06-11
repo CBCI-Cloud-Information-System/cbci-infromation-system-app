9be2c2ef89cd921b6bdfafe1dba8dd7e201be097
Mezmerizingcoder <mezmerizingcoder444@gmail.com>
2023-06-11 22:30:35 +0800

    Added Member Page & Expand Thumb

---
+++ resources/js/components/ExpandThumb/ExpandThumb.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import "./expand-thumb.css";
+
+
+function ExpandThumb() {
+    return (
+      <div className='expand-thumb'>
+
+      </div>
+    );
+}
+
+export default ExpandThumb;
---
+++ resources/js/pages/Members.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import "./members.css";
+import MemberThumb from '../components/MemberThumb/MemberThumb';
+import ExpandThumb from '../components/ExpandThumb/ExpandThumb';
+
+
+function Members() {
+    return (
+      <div className='member-wrapper'>
+        <div className='member-grid'>
+          <MemberThumb></MemberThumb>
+          <MemberThumb></MemberThumb>
+          <MemberThumb></MemberThumb>
+          <MemberThumb></MemberThumb>
+        </div>
+        <div id='expand-wrapper' className='expand-member-thumb-wrapper expand-hide'>
+          <ExpandThumb></ExpandThumb>
+        </div>
+      </div>
+    );
+}
+
+export default Members;
+
+if (document.getElementById('members')) {
+  ReactDOM.render(<Members />, document.getElementById('members'));
+}
