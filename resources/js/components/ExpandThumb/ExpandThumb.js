517b4c1cd0ccc13037d9caa44d9ec542881e4bdc
Mezmerizingcoder <mezmerizingcoder444@gmail.com>
2023-06-12 23:08:28 +0800

    Updated Expand Thumb added Member Details

--- resources/js/components/ExpandThumb/ExpandThumb.js
+++ resources/js/components/ExpandThumb/ExpandThumb.js
@@ -7,7 +7,17 @@ import "./expand-thumb.css";
 function ExpandThumb() {
     return (
       <div className='expand-thumb'>
-
+        <div className='expand-thumb-header'>
+          <h1 className='expand-thumb-title'>Member Details</h1>
+          <div className='expand-download-details-btn'>Download Details</div>
+        </div>
+        <div className='expand-thumb-details'>
+          <div className='expand-thumb-profile'></div>
+          <div className='expand-personal-details'>
+            <p className='expand-thumb-name'>Member Name</p>
+          </div>
+        </div>
+        
       </div>
     );
 }
