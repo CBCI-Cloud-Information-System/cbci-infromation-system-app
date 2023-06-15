18fd7c21cc68be5dadb0b063379cb11c8a4d1cae
Mezmerizingcoder <mezmerizingcoder444@gmail.com>
2023-06-15 16:48:53 +0800

    Integrated Member API

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,21 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\MemberController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Route::get('members', function(){
+//     return 'this is a fuction';
+// });
+
+Route::get('members', [MemberController::class, 'index']);
+Route::post('members', [MemberController::class, 'addMember']);
+Route::get('members/{id}', [MemberController::class, 'getMember']);
+Route::get('members/{id}/edit', [MemberController::class, 'editMember']);
+Route::get('members/{id}/edit', [MemberController::class, 'updateMember']);
+Route::get('members/{id}/delete', [MemberController::class, 'deleteMember']);
